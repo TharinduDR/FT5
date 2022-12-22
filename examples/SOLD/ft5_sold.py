50a3341518f5472f9215061984e303b26a90129e
TharinduDR <rhtdranasinghe@gmail.com>
2022-12-22 10:23:22 +0000

    001: Initial commit

--- examples/SOLD/ft5_sold.py
+++ examples/SOLD/ft5_sold.py
@@ -68,7 +68,7 @@ for i in range(FOLDS):
     model_args.best_model_dir = os.path.join(model_name_prefix, "outputs", "best_model")
     model_args.cache_dir = os.path.join(model_name_prefix, "cache_dir")
 
-    model = T5Model(model_type, model_name, args=model_args, use_cuda=torch.cuda.is_available(), cuda_device=1)
+    model = T5Model(model_type, model_name, args=model_args, use_cuda=torch.cuda.is_available(), cuda_device=cuda_device)
 
     # Train the model
     model.train_model(train_df, eval_data=eval_df)
