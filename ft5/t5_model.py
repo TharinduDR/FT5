bd2ce993653d70f9394b17334a4ce202eccd2368
TharinduDR <rhtdranasinghe@gmail.com>
2023-01-03 15:45:51 +0000

    001: Initial commit

--- ft5/t5_model.py
+++ ft5/t5_model.py
@@ -423,10 +423,10 @@ class T5Model:
         ):
             # Load in optimizer and scheduler states
             optimizer.load_state_dict(
-                torch.load(os.path.join(args.model_name, "optimizer.pt"))
+                torch.load(os.path.join(args.model_name, "optimizer.pt"), map_location='cuda:0')
             )
             scheduler.load_state_dict(
-                torch.load(os.path.join(args.model_name, "scheduler.pt"))
+                torch.load(os.path.join(args.model_name, "scheduler.pt"), map_location='cuda:0')
             )
 
         if args.n_gpu > 1:
